2d7de19338c6d95c8e6d7361343edb419ac01734
AlfredoRodriguez <126662592+Rodriguez-Alfredo@users.noreply.github.com>
2023-03-02 21:19:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#array to  store data in a list
+
+cars = ['toyota', 'honda','ford', 'chevrolet']
+
+for x in cars:
+
+  if x == 'toyota':
+    #special message for specific item on the list
+    print(x + ' - This car is on sale') 
+  
+  else:
+    print(x)
+
+#nest an if statment in a for loop
